3c60621bcfac7fc71d2769f253498b9954e19fff
SyrymJoli <syrym.joli@outlook.com>
2017-10-31 10:06:06 +0600

    add headr

--- src/components/Headr.jsx
+++ src/components/Headr.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
 
 export default class Headr extends Component {
   render() {
@@ -7,8 +9,18 @@ export default class Headr extends Component {
       <div className="Headr">
         <AppBar
           title="React"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          iconClassNameLeft="muidocs-icon-action-home"
+          iconElementLeft={
+            <div>
+                <FlatButton label="Docs" />
+                <FlatButton label="Tutorial" />
+                <FlatButton label="Community" />
+                <FlatButton label="Blog" />
+            </div>}
+
+          iconElementRight={<FlatButton label="GitHub" />}
         />
+        <div className="container" />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import App from './App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {fullBlack} from 'material-ui/styles/colors';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+    palette: {
+      textColor: fullBlack,
+    },
+    appBar: {
+      height: 50,
+    },
+  });
+
 ReactDOM.render(
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <App />
     </MuiThemeProvider>
     , document.getElementById('root')
